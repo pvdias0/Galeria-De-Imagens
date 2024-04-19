45262bd408899d828ec169950b46208301ae05bf
Andre-PI <andrelucas_PI@proton.me>
2024-04-19 15:41:10 -0300

    update

---
+++ main.js
@@ -0,0 +1,11 @@
+function selectLink(){
+    const links = ["PagAnimais","PagCidades","PagEsportes","PagPaisagens","PagTech","PedroDias"];
+}
+function clickedElement(){
+    
+}
+window.onclick
+
+document.querySelector("img.animals").addEventListener("click",function(ev){
+    window.location.assign("PagAnimais.html")
+});
