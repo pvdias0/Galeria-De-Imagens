72debd5bcae17df5fc99d2d3022f559fbf9abc6c
Andre-PI <andrelucas_PI@proton.me>
2024-04-20 22:15:58 -0300

    conclusão da homepage

--- main.js
+++ main.js
@@ -1,8 +1,16 @@
 function capitalize(string){
     return string.charAt(0).toUpperCase() + string.slice(1);
 }
-
+function containsSpecialChar(string){
+    let bool = string.indexOf("@") == -1 ? false : true;
+    console.log(string.indexOf("@"));
+    return bool;
+}
 document.getElementById("container").addEventListener('click' ,function(ev){
     window.location.assign("Pag"+ (capitalize(ev.target.className))+".html");
 });
 
+function btnClicked(){
+    let mailVar = document.getElementById("mail").value;
+    (mailVar != "" && containsSpecialChar(mailVar)) ? alert("Email cadastrado com sucesso !") : alert("Insira um endereço de Email válido !"); 
+}
