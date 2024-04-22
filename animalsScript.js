cfd030cc5eee93837b9203c687958549b6cd4cb7
Pedro Dias <158764917+pvdias0@users.noreply.github.com>
2024-04-22 21:26:01 +0000

    Todas as páginas com esqueleto adicionado

---
+++ animalsScript.js
@@ -0,0 +1,28 @@
+      const botoes = document.querySelectorAll('.botoesAparecem');
+      const links = document.querySelectorAll('.botao-link');
+
+      botoes.forEach((botao) => {
+         botao.addEventListener('mouseover', function () {
+            var elementoPai = this.parentNode;
+            var imgCorrespondente = elementoPai.previousElementSibling;
+            imgCorrespondente.classList.add('figuras-hover');
+         })
+         botao.addEventListener('mouseout', function () {
+            var elementoPai = this.parentNode;
+            var imgCorrespondente = elementoPai.previousElementSibling;
+            imgCorrespondente.classList.remove('figuras-hover')
+         })
+
+      })
+
+      document.addEventListener('DOMContentLoaded', function(){
+         const botaoSeta = document.querySelector('#botaoSeta');
+         const ancoraSeta = document.getElementById('animalSection');
+
+         botaoSeta.addEventListener('click', function(){
+            window.scrollTo({
+               top:ancoraSeta.offsetTop, behavior:"smooth"
+            });
+         })
+         
+      })
