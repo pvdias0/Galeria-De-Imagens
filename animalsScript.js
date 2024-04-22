17c28fadb609c0a2730a560a8f237ec46afd52cc
Andre Lucas <141793916+Andre-PI@users.noreply.github.com>
2024-04-22 18:23:38 +0000

    nenhuma mudança

--- animalsScript.js
+++ animalsScript.js
@@ -1,30 +1,6 @@
-let images = document.querySelectorAll(".figuras");
-let imageChunkSize = 5; // Adjust this value based on performance needs
-
-document.addEventListener("DOMContentLoaded", () => {
-  fetch("links.json").then(response => response.json()).then(data => {
-    let imageCount = data.animais.length;
-    let processedImages = 0;
-
-    function loadImages(startIndex) {
-      for (let i = startIndex; i < Math.min(startIndex + imageChunkSize, imageCount); i++) {
-        const image = new Image();
-        image.src = data.paisagens[i]; // Load image outside loop
-
-        image.onload = function() {
-          images[i].src = this.src;
-        };
-
-        processedImages++;
-      }
-
-      if (processedImages < imageCount) {
-        setTimeout(function() {
-          loadImages(processedImages);
-        }, 1000); // Adjust timeout as needed
-      }
+fetch("links.json").then(response => response.json()).then(data => {
+    let images = document.querySelectorAll(".figuras");
+    for(let i =0;i < images.length;i++){
+        images[i].src = data.paisagens[i];
     }
-
-    loadImages(0);
-  });
-});
+})
